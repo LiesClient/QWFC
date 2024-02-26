138786b44f39271fb6ac28dd49d2385a1bf3fc10
lyinglint <77509842+LiesClient@users.noreply.github.com>
2024-02-26 09:46:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,131 +1,191 @@
+const outputEl = document.getElementById("output");
 const canvas = document.getElementById("display");
-const pcanvas = document.getElementById("pattern");
 const ctx = canvas.getContext("2d");
-const pctx = pcanvas.getContext("2d");
 
-canvas.width = dsize;
-canvas.height = dsize;
-pcanvas.width = psize;
-pcanvas.height = psize;
 
-let pattern = new mat(() => 0, psize, psize), display;
+let cells;
+let targetFPS = 60;
+let loopCount = 1;
+let targetFrameTime = 1000 / targetFPS;
+let lastTime = performance.now();
 
-pcanvas.addEventListener("click", e => {
-  let rect = pcanvas.getBoundingClientRect();
-  let x = e.x - rect.left;
-  let y = e.y - rect.top;
-  let gx = Math.floor(x / rect.width * psize);
-  let gy = Math.floor(y / rect.height * psize);
-  pattern[gx][gy] = (1 + pattern[gx][gy]) % colors.length;
-  init();
-});
+async function init() {
+  cells = new Cells(DISPLAY_WIDTH, DISPLAY_HEIGHT, ID, THREAD_COUNT);
+  canvas.width = DISPLAY_WIDTH;
+  canvas.height = DISPLAY_HEIGHT;
 
-// main structure functions
-function init() {
-  display = new mat(() => new Cell(), dsize, dsize);
+  document.addEventListener("click", e => {
+    cells.collapseRandom(PROBABILITIES);
+  });
 
-  let rules = [];
+  Object.keys(TEXTURES).forEach(async txt => {
+    if (!txt) return;
+    await txt.complete == true;
+  });
 
-  for (let x = 1; x < psize - 1; x++)
-    for (let y = 1; y < psize - 1; y++) {
-      let rule = new Rule(sample(pattern, x, y));
-      rule.setDispRef(display);
-      rules.push(rule);
+  document.addEventListener("keydown", e => {
+    if (e.key == " ") {
+      // cells.reset();
+      // perfBench = null;
     }
+  });
 
-  for (let x = 0; x < dsize; x++)
-    for (let y = 0; y < dsize; y++) {
-      let cell = display[x][y];
-      cell.setPosition(x, y);
-      cell.setDispRef(display);
-      cell.setRuleRef(rules);
-    }
+  // let res = [16, 12];
+  // let res = [36, 24];
+  // let times = 1;
 
-  solve();
+  // let tArr = [];
+
+  // log(`benching at (${res[0]}, ${res[1]}), ${times} times`);
+
+  // let runBench = () => {
+  //   let t = perfBench(res[0], res[1]);
+  //   tArr.push(t);
+  //   log(`#${tArr.length}: ${Math.round(t * 10) / 10}ms`);
+
+  //   if (tArr.length < times) {
+  //     setTimeout(runBench, 100);
+  //   } else {
+  //     let tAvg = tArr.reduce((a, b) => a + b, 0) / tArr.length;
+
+  //     log(`benchmarks complete`);
+  //     log(`average time: ${Math.round(tAvg * 10) / 10}ms`);
+  //   }
+  // }
+
+  // runBench();
+
+  lastTime = performance.now();
+  loop();
 }
 
-function draw() {
-  ctx.clearRect(0, 0, dsize, dsize);
-  ctx.clearRect(0, 0, psize, psize);
+function log(text) {
+  let el = document.createElement("p");
+  el.textContent = text;
+  outputEl.appendChild(el);
+}
 
-  for (let x = 0; x < psize; x++)
-    for (let y = 0; y < psize; y++) {
-      let index = pattern[x][y];
+function perfBench(w, h) {
+  let name = `Perf Benching for ${w}, ${h}`
+  console.log("Starting perf bench...");
+  console.time(name);
+  let t0 = performance.now();
+  
+  cells = new Cells(w, h, ID, THREAD_COUNT);
 
-      pctx.fillStyle = colors[index];
-      pctx.fillRect(x, y, 1, 1);
+  function runIter() {
+    cells.collapseRandom(PROBABILITIES);
+    cells.collapseAll(cellsComplete => {
+      if (!cellsComplete)
+        runIter();
+    });
+  }
+
+  try {
+    runIter();
+  } catch (e) {
+    runIter();
+  }
+
+  console.log("iterations complete");
+  let t1 = performance.now();
+  console.timeEnd(name);
+  drawFinale();
+
+  return t1 - t0;
+}
+
+function loop() {
+  ctx.clearRect(0, 0, DISPLAY_WIDTH, DISPLAY_HEIGHT);
+
+  let t = performance.now();
+  let dt = t - lastTime;
+  lastTime = t;
+
+  if (dt < targetFrameTime) {
+    loopCount ++;
+  } 
+
+  if (dt > targetFrameTime) {
+    loopCount --;
+  }
+
+  if (loopCount <= 0) {
+    loopCount = 1;
+  }
+  
+  for (let x = 0; x < DISPLAY_WIDTH; x++) {
+    for (let y = 0; y < DISPLAY_HEIGHT; y++) {
+      let cell = cells.get(x, y);
+      let clr = [0, 0, 0];
+  
+      // for (let i = 0; i < cell.possible.length; i++) {
+      //   clr[0] += cell.possible[i].color[0];
+      //   clr[1] += cell.possible[i].color[1];
+      //   clr[2] += cell.possible[i].color[2];
+      // }
+
+      // clr[0] /= cell.possible.length;
+      // clr[1] /= cell.possible.length;
+      // clr[2] /= cell.possible.length;
+
+      for (let i = 0; i < cell.possible.length; i++) {
+        clr[0] += COLORS[ID[cell.possible[i]]].color[0];
+        clr[1] += COLORS[ID[cell.possible[i]]].color[1];
+        clr[2] += COLORS[ID[cell.possible[i]]].color[2];
+      }
+
+      clr[0] /= cell.possible.length;
+      clr[1] /= cell.possible.length;
+      clr[2] /= cell.possible.length;
+
+      ctx.fillStyle = `rgb(${clr.join(",")})`;
+      ctx.fillRect(x, y, 1, 1);
     }
+  }
 
-  for (let x = 0; x < dsize; x++)
-    for (let y = 0; y < dsize; y++) {
-      let cell = display[x][y];
-      ctx.globalAlpha = 1 / cell.possibilities.length;
+  for (let i = 0; i < loopCount; i++) {
+    cells.collapseRandom(PROBABILITIES);
+    if (cells.collapseAll()) return drawFinale(cells);
+  }
 
-      for (let i = 0; i < cell.possibilities.length; i++) {
-        ctx.fillStyle = colors[cell.possibilities[i]];
-        ctx.fillRect(x, y, 1, 1);
+  requestAnimationFrame(loop);
+}
+
+// adds "textures"
+function drawFinale() {
+  // todo: make this do smthn
+  console.log("rendering final texture");
+  canvas.width = cells.width * TEXTURE_SIZE;
+  canvas.height = cells.height * TEXTURE_SIZE;
+
+  for (let x = 0; x < cells.width; x++) {
+    for (let y = 0; y < cells.height; y++) {
+      let cell = cells.get(x, y);
+      let poss = ID[cell.possible[0]];
+      let txt = TEXTURES[poss];
+
+      if (txt) {
+        ctx.drawImage(txt, x * TEXTURE_SIZE, y * TEXTURE_SIZE);
+      } else {
+        ctx.fillStyle = `rgb(${poss.color.join(",")})`;
+        ctx.fillRect(x * TEXTURE_SIZE, y * TEXTURE_SIZE, TEXTURE_SIZE, TEXTURE_SIZE);
       }
     }
-
-  ctx.globalAlpha = 1;
-}
-
-function solve() {
-  recalculate();
-
-  while (totalEntropy() != 0) {
-    iterate();
-    recalculate();
   }
 
-  recalculate();
-
-  draw();
+  console.log("drawFinale complete");
+  evaluateProbs();
 }
 
-function iterate() {
-  let x = Math.floor(Math.random() * dsize);
-  let y = Math.floor(Math.random() * dsize);
+function evaluateProbs() {
+  let probs = {};
 
-  if (display[x][y].possibilities.length == 1) return iterate();
-  display[x][y].collapse();
+  Object.keys(PROBABILITIES).forEach(key => {
+    probs[key] = 0;
+  });
 }
 
-function recalculate() {
-  let offsetx = Math.floor(Math.random() * dsize);
-  let offsety = Math.floor(Math.random() * dsize);
-  
-  for (let x = 0; x < dsize; x++)
-    for (let y = 0; y < dsize; y++) {
-      let cell = display[(x + offsetx) % dsize][(y + offsety) % dsize];
-      cell.recalculate();
-    }
-}
-
-function totalEntropy() {
-  let e = 0;
-
-  for (let x = 0; x < dsize; x++)
-    for (let y = 0; y < dsize; y++){
-      e += display[x][y].possibilities.length != 1;
-    }
-
-  return e;
-}
-
-// helpers
-function mat(fn, width, height) {
-  let arr = new Array(width).fill(0);
-
-  for (let i = 0; i < width; i++) {
-    arr[i] = new Array(height).fill(0);
-
-    for (let j = 0; j < height; j++)
-      arr[i][j] = fn();
-  }
-
-  return arr;
-}
-
-
-init();
+setTimeout(() => {
+  init();
+}, 1000);
